bfe035d4f160a33a1c9ef7f78ba0304c4ae466af
gazdagergo <info@gazdagergo.com>
2024-05-23 06:58:11 +0200

    dpga header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Jost } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const jost = Jost({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jost.className}>{children}</body>
+      <body className={jost.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- components/ActivityForm.jsx
+++ components/ActivityForm.jsx
@@ -1,7 +1,7 @@
 const ActivityForm = () => {
   return (
     <>
-      <h2 className="my-4 mt-0 text-4xl leading-tight text-primary text-purple font-normal">
+      <h2 className="my-8  text-4xl leading-tight text-primary text-purple font-normal">
         Register your activity
       </h2>
 
@@ -49,7 +49,7 @@ const ActivityForm = () => {
         />
       </div>
 
-      <div className="mt-2">
+      <div className="mt-8">
         <button
           type="button"
           className="inline-block rounded bg-blue-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-blue-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0"
