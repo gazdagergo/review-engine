4c5110b970dd468824adae1bce8899e978332c25
gazdagergo <info@gazdagergo.com>
2024-06-12 20:57:31 +0200

    fix non-event related text

--- components/EventRegistrationForm.jsx
+++ components/EventRegistrationForm.jsx
@@ -66,7 +66,7 @@ const ActivityForm = () => {
         <textarea
           className="relative m-0 block flex-auto rounded-e border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-white dark:placeholder:text-neutral-200 dark:focus:border-primary"
           aria-label="Activity details"
-          placeholder="Activity details"
+          placeholder="Other information"
           defaultValue={""}
         />
       </div>
@@ -77,13 +77,13 @@ const ActivityForm = () => {
           onClick={handleSubmit}
           className="inline-block rounded bg-blue-500 text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] hover:bg-blue-600 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-blue-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] active:bg-blue-700 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0"
         >
-          Submit activity
+          Submit your registration
         </button>
       </div>
 
       {status === 'success' && (
         <SuccessMessage className="mt-8 cursor-pointer" onClick={() => setStatus(null)}>
-          Activity submitted
+          Registration successful. Please, check your inbox.
         </SuccessMessage>
       )}
     </>
